219f990b53fbdcffb8988aa62386acdd94c1cf3b
Anjali Trace <anjali.trace13@gmail.com>
2019-08-30 16:08:07 +0100

    Added help and useage along with more robust arg handling.

--- bash.sh
+++ bash.sh
@@ -1,4 +1,13 @@
-# Pass arg 1 as day number 
+#!/bin/bash
+
+USAGE="
+Useage: $0 [-h] [-a] [n]
+Sets up directories for a given day or all days of advent of code. Creates a directory day[n] in current dir with empty files for associated text input and python script.
+
+    -h    show this help message.
+    -a    set up directories for all remaining days (not already existing).
+    n     input number [0-25] to setup directory for day n.
+"
 
 make_dir () {
     mkdir $1
@@ -16,7 +25,9 @@ setup_dir () {
     fi
 }
 
-if [ $1 == 'a' ]; then
+if [ $1 == '-h' ]; then
+    echo "$USAGE"
+elif [ $1 == '-a' ]; then
     COUNT=1
     while [ $COUNT -le 25 ]; do
         setup_dir $COUNT
@@ -26,5 +37,6 @@ if [ $1 == 'a' ]; then
 elif [ $1 -le 25 ] && [ $1 -gt 0 ]; then
     setup_dir $1
 else
-    echo "Please enter a number in the range 1-25"
+    echo "Please supply a number in the range 1-25. For help please use $0 -h"
 fi
+
